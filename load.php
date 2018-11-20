a571dac90df856beae9448a19004a1da58741db3
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2018-11-19 23:57:24 -0300

    Changed Autoloader variable

--- load.php
+++ load.php
@@ -1,9 +1,9 @@
 <?php
 
-$twcal = __DIR__.'/vendor/autoload.php';
+$autoloader = __DIR__.'/vendor/autoload.php';
 
-if (file_exists($twcal) && is_readable($twcal)) {
-    include_once "$twcal";
+if (file_exists($autoloader) && is_readable($autoloader)) {
+    include_once "$autoloader";
 } else {
     throw new Exception('Composer has not been installed, or autoloader is not present.');
 }
