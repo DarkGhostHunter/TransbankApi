2ebdfec35a58053891fe513e250badc7171da247
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2020-05-11 17:38:18 -0400

    Fixed f*cked up validation.

---
+++ tests/Feature/Integration/OnepayIntegrationTransactionsTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature\Integration;
+
+use DarkGhostHunter\TransbankApi\Exceptions\Onepay\OnepayClientException;
+use DarkGhostHunter\TransbankApi\Exceptions\Onepay\OnepayResponseException;
+use PHPUnit\Framework\TestCase;
+use DarkGhostHunter\TransbankApi\Exceptions\Onepay\CartEmptyException;
+use DarkGhostHunter\TransbankApi\Exceptions\Onepay\CartNegativeAmountException;
+use DarkGhostHunter\TransbankApi\Onepay;
+use DarkGhostHunter\TransbankApi\Transbank;
+
+class OnepayIntegrationTransactionsTest extends TestCase
+{
+    /** @var Onepay */
+    protected $onepay;
+
+    protected function setUp() : void
+    {
+        $transbank = Transbank::make('integration');
+
+        $transbank->setDefaults('onepay', [
+            'channel'               => 'mobile',
+            'generateOttQrCode'     => true,
+            'callbackUrl'           => 'http://app.com/onepay/result',
+            'appScheme'             => 'my-app://onepay/result',
+        ]);
+
+        $this->onepay = Onepay::fromConfig($transbank);
+    }
+
+    public function testCreatesCartInTransbank()
+    {
+        $cart = $this->onepay->createCart([
+            'channel' => 'web',
+            'items' => [
+                'description' => 'Zapatos',
+                'quantity' =>  1,
+                'amount' => 15000
+            ]
+        ]);
+
+        $this->assertTrue($cart->isSuccess());
+        $this->assertNotEmpty($cart->ott);
+        $this->assertNotEmpty($cart->externalUniqueNumber);
+        $this->assertNotEmpty($cart->qrCodeAsBase64);
+        $this->assertNotEmpty($cart->issuedAt);
+        $this->assertNotEmpty($cart->signature);
+    }
+
+    public function testExceptionOnChannelAppWithoutScheme()
+    {
+        $this->expectException(OnepayResponseException::class);
+
+        $this->onepay->createCart([
+            'channel' => 'app',
+            'appScheme' => null,
+            'items' => [
+                'description' => 'Zapatos',
+                'quantity' =>  1,
+                'amount' => 15000
+            ]
+        ]);
+    }
+
+    public function testCreatedCartHidesSignatureOnSerialization()
+    {
+        $cart = $this->onepay->createCart([
+            'items' => [
+                'description' => 'Zapatos',
+                'quantity' =>  1,
+                'amount' => 15000
+            ]
+        ]);
+
+        $this->assertFalse(strpos($cart->toJson(), 'signature'));
+        $this->assertFalse(strpos((string)$cart, 'signature'));
+    }
+
+    public function testExceptionOnCartWithZeroAmount()
+    {
+        $this->expectException(CartNegativeAmountException::class);
+
+        $cart = $this->onepay->createCart([
+            'items' => [
+                'description' => 'Zapatos',
+                'quantity' =>  10,
+                'amount' => -15000
+            ]
+        ]);
+    }
+
+    public function testExceptionOnEmptyCart()
+    {
+        $this->expectException(CartEmptyException::class);
+
+        $cart = $this->onepay->createCart([
+            'items' => []
+        ]);
+    }
+}
---
+++ tests/Feature/Integration/WebpayIntegrationTransactionsTest.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace Tests\Feature\Integration;
+
+use PHPUnit\Framework\TestCase;
+use DarkGhostHunter\TransbankApi\Transbank;
+use DarkGhostHunter\TransbankApi\Webpay;
+use DarkGhostHunter\TransbankApi\Responses\WebpayOneclickResponse;
+use DarkGhostHunter\TransbankApi\Responses\WebpayPlusMallResponse;
+use DarkGhostHunter\TransbankApi\Responses\WebpayPlusResponse;
+use DarkGhostHunter\TransbankApi\Exceptions\Webpay\InvalidWebpayTransactionException;
+
+class WebpayIntegrationTransactionsTest extends TestCase
+{
+
+    /** @var Webpay */
+    protected $webpay;
+
+    protected function setUp()
+    {
+        $transbank = Transbank::make('integration');
+
+        $transbank->setDefaults('webpay', [
+            'plusReturnUrl'         => 'http://app.com/webpay/result',
+            'plusFinalUrl'          => 'http://app.com/webpay/receipt',
+            'plusMallReturnUrl'     => 'http://app.com/webpay/mall/result',
+            'plusMallFinalUrl'      => 'http://app.com/webpay/mall/receipt',
+            'oneclickResponseURL'   => 'http://app.com/webpay/registration',
+        ]);
+
+        $this->webpay = Webpay::fromConfig(
+            $transbank
+        );
+    }
+
+    /**
+     * @group selenium
+     */
+    public function testCommitsPlusNormal()
+    {
+        $normal = $this->webpay->createNormal([
+            'returnUrl' => 'http://app.com/webpay/return',
+            'finalUrl' => 'http://app.com/webpay/final',
+            'amount' => 2000,
+            'buyOrder' => 'testBuyOrder',
+        ]);
+
+        $this->assertInstanceOf(WebpayPlusResponse::class, $normal);
+        $this->assertTrue(is_string($normal->token));
+        $this->assertTrue(is_string(filter_var($normal->url, FILTER_VALIDATE_URL)));
+
+    }
+
+    public function testSendsInvalidPlusNormal()
+    {
+        $this->expectException(InvalidWebpayTransactionException::class);
+
+        $normal = $this->webpay->createNormal([
+            'returnUrl' => 'invalidReturnUrl',
+            'finalUrl' => 'invalidFinalUrl',
+            'amount' => -2000,
+            'buyOrder' => -9999,
+        ]);
+    }
+
+    public function testCreatesPlusMallNormal()
+    {
+        $normal = $this->webpay->createMallNormal([
+            'buyOrder' => 'parent-order-123123',
+            'items' => [
+                [
+                    'commerceCode' => 597044444402,
+                    'amount' => 2000,
+                    'buyOrder' => '597044444402-1',
+                    'sessionId' => 'session-id-1'
+                ],
+                [
+                    'commerceCode' => 597044444403,
+                    'amount' => 2000,
+                    'buyOrder' => '597044444403-1',
+                    'sessionId' => 'session-id-2'
+                ],
+
+            ]
+        ]);
+
+        $this->assertInstanceOf(WebpayPlusMallResponse::class, $normal);
+        $this->assertTrue(is_string($normal->token));
+        $this->assertTrue(is_string(filter_var($normal->url, FILTER_VALIDATE_URL)));
+    }
+
+    public function testCommitsInvalidPlusMallNormal()
+    {
+        $this->expectException(InvalidWebpayTransactionException::class);
+
+        $normal = $this->webpay->createMallNormal([
+            'buyOrder' => 'parent-order-123123',
+            'items' => [
+                [
+                    'storeCode' => 597044444402,
+                    'amount' => -2000,
+                    'buyOrder' => -9999,
+                    'sessionId' => -9999
+                ],
+                [
+                    'storeCode' => 597044444403,
+                    'amount' => -2000,
+                    'buyOrder' => -9999,
+                    'sessionId' => -9999
+                ],
+            ]
+        ]);
+    }
+
+    public function testCommitsNotFoundPlusCapture()
+    {
+        $this->expectException(InvalidWebpayTransactionException::class);
+
+        $normal = $this->webpay->createCapture([
+            'authorizationCode' => '!found',
+            'captureAmount' => 2000,
+            'buyOrder' => 'testBuyOrder',
+            'captureAmount ' => 2000,
+        ]);
+
+        $mall = $this->webpay->createMallCapture([
+            'sessionId' => 'client-session-id-88',
+            'buyOrder' => 'myOrder#16548',
+            'amount' => 1000,
+            'commerceOrder' => 597044444402
+        ]);
+    }
+
+    public function testCommitsInvalidNullify()
+    {
+        $this->expectException(InvalidWebpayTransactionException::class);
+
+        $nullify = $this->webpay->createNullify([
+            'authorizationCode' => '03fr4E',
+            'authorizedAmount' => 19990,
+            'buyOrder' => 'store-order-32154',
+            'nullifyAmount' => 10000,
+        ]);
+    }
+
+    public function testCreatesOneclickRegistration()
+    {
+        $registration  = $this->webpay->createRegistration([
+            'username' => 'appusername',
+            'email' => 'username@gmail.com',
+            'responseUrl' => 'https://app.com/oneclick/result'
+        ]);
+
+        $this->assertInstanceOf(WebpayOneclickResponse::class, $registration);
+        $this->assertTrue(is_string($registration->token));
+        $this->assertTrue(is_string(filter_var($registration->url, FILTER_VALIDATE_URL)));
+    }
+
+    public function testCommitsInvalidOneclickUnregistration()
+    {
+        $unregistration  = $this->webpay->createUnregistration([
+            'username' => 'appusername',
+            'tbkUser' => 'tbkUser',
+        ]);
+
+        $this->assertFalse($unregistration->isSuccess());
+    }
+
+    public function testCommitsInvalidCharge()
+    {
+        $this->expectException(InvalidWebpayTransactionException::class);
+
+        $charge = $this->webpay->createCharge([
+            'amount' => 9990,
+            'buyOrder' => 7,
+            'tbkUser' => 'tbkUser',
+            'username' => 'appusername',
+        ]);
+    }
+
+
+    public function testCommitsInvalidOneclickReverseCharge()
+    {
+        $reverse = $this->webpay->createReverseCharge([
+            'buyOrder' => 20202020120001001
+        ]);
+
+        $this->assertFalse($reverse->isSuccess());
+    }
+
+
+}
